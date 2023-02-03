5e9bfed9e11ffbb4ff9778b04710bbd14c9da92d
unknown <vipulr6111@gmail.com>
2023-02-03 20:56:37 +0530

    architecture ready

---
+++ src/components/nav/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./navbar.css";
+import tiffin from "../../assests/tiffin.png";
+
+export default function Navbar() {
+  return (
+    <div>
+      <header class="text-gray-700 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <img src={tiffin} alt="logo" className="logo h-16" />
+            <span class="ml-3 text-xl">Tiffin</span>
+          </a>
+          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-gray-900 cursor-pointer">
+              Login As Provider
+            </a>
+          </nav>
+          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Login
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+          <button class="inline-flex items-center bg-gray-100 border-0 py-1 mx-2 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            SignUp
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
